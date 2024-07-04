a3868ed3fe5e3852df6268a82fd8532fb7a6d595
daniel-sfranco <daniel.sfranco346@outlook.com>
2024-07-04 18:23:03 -0300

    Adding music to directory

--- audio.py
+++ audio.py
@@ -9,7 +9,7 @@ correct = pygame.mixer.Sound('./audio/correct.wav')
 level_complete = pygame.mixer.Sound('./audio/level_complete.wav')
 choice = pygame.mixer.Sound('./audio/choice.wav')
 
-#setting some volumes
+#setting volumes
 points.set_volume(0.7)
 select.set_volume(0.5)
 choice.set_volume(0.5)
